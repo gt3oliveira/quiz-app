9d04573d534a876c3b71cfec473c38ccb379c865
Gustavo Tavares <gt3oliveira@gmail.com>
2023-09-09 21:14:07 -0300

    Atualizando o layout: line 8-15

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,13 @@ import { Roboto } from 'next/font/google'
 const roboto = Roboto({ subsets: ['latin'], weight: ['100', '300', '500', '900'] })
 
 export const metadata: Metadata = {
-	title: 'Quiz App'
-};
+	title: 'Quiz Brasil 🧭',
+	description: 'Descubra as curiosidades do mapa do Brasil com nosso quiz interativo e conquiste seu top no ranking!',
+	openGraph: {
+		title: 'Quiz Brasil 🧭',
+		images: '@/assets/images/logo-quiz.png'
+	}
+}
 
 export default function RootLayout({
 	children,
@@ -15,7 +20,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" suppressHydrationWarning>			
+		<html lang="en" suppressHydrationWarning>
 			<body className={`${roboto.className}`}>
 				<Providers>
 					<main>
